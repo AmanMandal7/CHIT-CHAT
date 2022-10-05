9d2ac2c1b03c60d75cabb8d0b4eb81ac807cfb46
AmanGH009 <aman.mandal.71334@gmail.com>
2022-10-05 11:53:57 +0530

    timestamp error

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
+const timestamps = require('mongoose-timestamp')
 
 const userModel = mongoose.model({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    pic: { type: String, required: true, default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" },
+    pic: { type: String, default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" },
 
 },
     {
-        timestamps: true
+        timestamps: true,
     }
 );
 
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const chats = require('./data/data');
 const connectDB = require('./config/db');
-
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 dotenv.config();
@@ -13,15 +13,7 @@ app.get("/", (req, res) => {
     res.send("API is running !");
 })
 
-app.get("/api/chat", (req, res) => {
-    res.send(chats)
-})
-
-app.get("/api/chat/:id", (req, res) => {
-    // console.log(req.params.id);
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-});
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
